7b3957a3588f9118d29146197ef45dd5a8e8e0eb
Xavthaman <44090635+Xavthaman@users.noreply.github.com>
2019-05-05 17:12:46 -0500

    Added DB

--- webapp.sh
+++ webapp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-resourcegroup=$1
-appname=$2
+ resourcegroup=$1
+ appname=$2
+ storage_acc=$3
+ container_name=$4
+ blobName=$5
+ databasename=$6
 
 # Replace the following URL with a public GitHub repo URL
 gitrepo=https://github.com/xavierosierra/revature-p1.git
@@ -19,11 +23,14 @@ az appservice plan create \
 
 #Creates storage account 5/4/2019
 az storage account create \
---name $appname
---resource-group $resourcegroup \
+--name $storage_acc
+-g $resourcegroup \
 --location southcentralus \
 --sku Standard_GRS \
---kind StorageV2
+--kind BlobStorage
+
+#After creating Storage Account we need to create a CONTAINER in the blob  5/4/2019
+az storage container create --name $storage_acc --fail-on-exist
 
 # Create a web app.
 az webapp create \
@@ -31,24 +38,52 @@ az webapp create \
 --resource-group $resourcegroup \
 --plan $appname
 
-#Create a deployment slot with the name "staging".
-az webapp deployment slot create \
---name $appname \
---resource-group $resourcegroup \
---slot staging
+# Create a SQL API Cosmos DB account with session consistency and multi-master enabled 5/5/2019
+az cosmosdb create \
+    --resource-group $resourcegroup \
+    --name $appname \
+    --kind GlobalDocumentDB \
+    --location "South Central US"
+    --default-consistency-level "Session" \
+    --enable-multiple-write-locations true
 
-# Deploy sample code to "staging" slot from GitHub.
-az webapp deployment source config --name $appname --resource-group $resourcegroup \
---slot staging --repo-url $gitrepo --branch master --manual-integration
 
-# Copy the result of the following command into a browser to see the staging slot.
-echo http://$webappname-staging.azurewebsites.net
+# Create a database
+az cosmosdb database create \
+    --resource-group $resourcegroup \
+    --name $appname \
+    --db-name $databasename
 
-# Swap the verified/warmed up staging slot into production.appname
-az webapp deployment slot swap \
---name $appname \
---resource-group $resourcegroup \
---slot staging
 
-# Copy the result of the following command into a browser to see the web app in the production slot.
-echo http://$webappname.azurewebsites.net
+# Create a SQL API container with a partition key and 1000 RU/s
+az cosmosdb collection create \
+    --resource-group $resourcegroup \
+    --collection-name $container_name \
+    --name $appname \
+    --db-name $databasename \
+    --partition-key-path /mypartitionkey \
+    --throughput 1000
+
+
+
+# #Create a deployment slot with the name "staging".
+# az webapp deployment slot create \
+# --name $appname \
+# --resource-group $resourcegroup \
+# --slot staging
+
+# # Deploy sample code to "staging" slot from GitHub.
+# az webapp deployment source config --name $appname --resource-group $resourcegroup \
+# --slot staging --repo-url $gitrepo --branch master --manual-integration
+
+# # Copy the result of the following command into a browser to see the staging slot.
+# echo http://$webappname-staging.azurewebsites.net
+
+# # Swap the verified/warmed up staging slot into production.appname
+# az webapp deployment slot swap \
+# --name $appname \
+# --resource-group $resourcegroup \
+# --slot staging
+
+# # Copy the result of the following command into a browser to see the web app in the production slot.
+# echo http://$webappname.azurewebsites.net
