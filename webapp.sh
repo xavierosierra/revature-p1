4ba5276a1ea10eb5143fa6ebe01bd0ea4f1511b0
Xavthaman <44090635+Xavthaman@users.noreply.github.com>
2019-05-06 14:53:08 -0500

    last push

--- webapp.sh
+++ webapp.sh
@@ -3,22 +3,17 @@
  appname=$2
  storage_acc=$3
  container_name=$4
- blobName=$5
- databasename=$6
-
-# Replace the following URL with a public GitHub repo URL
-gitrepo=https://github.com/xavierosierra/revature-p1.git
-webappname=mywebapp$RANDOM
+ databasename=$5
 
 # Create a resource group.
 az group create --location southcentralus --name $resourcegroup
 
-# Create an App Service plan in  tier PremiumV2 because it can Autoscale up to 20 instances
+# Create an App Service plan 
 az appservice plan create \
 -n $appname \
 -g $resourcegroup \
 --is-linux -l "South Central US" \
---sku P1V2 \
+--sku B1 \
 --number-of-workers 3
 
 #Creates storage account 5/4/2019
@@ -30,7 +25,7 @@ az storage account create \
 --kind BlobStorage
 
 #After creating Storage Account we need to create a CONTAINER in the blob  5/4/2019
-az storage container create --name $storage_acc --fail-on-exist
+az storage container create --name $storage_acc
 
 # Create a web app.
 az webapp create \
@@ -66,24 +61,3 @@ az cosmosdb collection create \
 
 
 
-# #Create a deployment slot with the name "staging".
-# az webapp deployment slot create \
-# --name $appname \
-# --resource-group $resourcegroup \
-# --slot staging
-
-# # Deploy sample code to "staging" slot from GitHub.
-# az webapp deployment source config --name $appname --resource-group $resourcegroup \
-# --slot staging --repo-url $gitrepo --branch master --manual-integration
-
-# # Copy the result of the following command into a browser to see the staging slot.
-# echo http://$webappname-staging.azurewebsites.net
-
-# # Swap the verified/warmed up staging slot into production.appname
-# az webapp deployment slot swap \
-# --name $appname \
-# --resource-group $resourcegroup \
-# --slot staging
-
-# # Copy the result of the following command into a browser to see the web app in the production slot.
-# echo http://$webappname.azurewebsites.net
